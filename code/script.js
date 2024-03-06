2ad91cab4a7de073cb7ff4c0c1e5be0146ba3f7d
Arjay_Villa <a.villa00397@student.tsu.edu.ph>
2024-03-06 12:04:41 +0800

    TAPOS

---
+++ code/script.js
@@ -0,0 +1,3 @@
+function myFunction(){
+  document.getElementById("parachanged").innerHTML = "Just dont give up trying to do what you really want to do. Where there is love and inspiration, I dont think you can go wrong.";
+}
